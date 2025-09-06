72ba797fb1edb53d586bd0e5e0e7000af67a620c
Ferri Krisdiantoro <ferri.krisdiantoro.study@gmail.com>
2025-09-06 07:05:20 +0700

    fix detail emiten

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // >>> PENTING: sesuaikan dengan prefix tempat app kamu di-hosting
+  // Jika Apache/Nginx mem-proxy di /app, set basePath ke "/app"
+  basePath: "/app",
+
+  // Jangan set assetPrefix kalau proxy sudah meneruskan /app/_next/*
+  // assetPrefix: "/app", // HANYA kalau proxy/CDN kamu butuh ini
 };
 
 export default nextConfig;
